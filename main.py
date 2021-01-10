4de7f0b4bc673b1e44adc6459b9fc31d74f719bc
Vladimir Bogdan <vv.bogdan98@yandex.ru>
2021-01-10 15:53:54 +0700

    StyleGan2 Latent Directions

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 import sys
 import time
 
+sys.path.append('.')
+
 import cv2
 import dlib
 import torch
 import numpy as np
 from tqdm import tqdm
 from PIL import Image
-from inference import *
 from datetime import datetime
 from argparse import Namespace
 import torchvision.transforms as transforms
@@ -17,13 +18,17 @@ from models.psp import pSp
 from utils.common import tensor2im, log_input_image
 from scripts.align_all_parallel import align_face
 
-PRETRAINED_WEIGHT = '/root/workspace/stylegan_merlinapp_service/psp_ffhq_encode.pt'
-TEST_DATA_DIR = '/root/workspace/stylegan_merlinapp_service/test_data'
-RESULT_DATA_PATH = '/root/workspace/stylegan_merlinapp_service/test_data_res'
-LATENT_DIRECTION_PATH = '/root/workspace/stylegan_merlinapp_service/latent_directions'
-FACE_LANDMARKS_PATH = '/root/workspace/stylegan_merlinapp_service/shape_predictor_68_face_landmarks.dat'
+PRETRAINED_WEIGHT = 'psp_ffhq_encode.pt'
+TEST_DATA_DIR = './test_data'
+RESULT_DATA_PATH = './test_data_res'
+LATENT_DIRECTION_PATH = './latent_directions'
+FACE_LANDMARKS_PATH = './shape_predictor_68_face_landmarks.dat'
 ALIGN_IMAGE = True
 COEFFS = [-15., -12., -9., -6., -3., 0., 3., 6., 9., 12.]
+
+AGE_LATENT_DIR = './latent_directions/age.npy'
+SMILE_LATENT_DIR = './latent_directions/smile.npy'
+GENDER_LATENT_DIR = './latent_directions/gender.npy'
 
 INFERENCE_DATA_TRANSFORMS = transforms.Compose([
     transforms.Resize((256, 256)),
@@ -67,18 +72,16 @@ def main():
 
         with torch.no_grad():
             inference_time = time.time()
-            res_img, latents_dir = net(inp_img.to('cuda').float().unsqueeze(0), randomize_noise=False, return_latents=True)
+            res_img, latents_dir = net(inp_img.to('cuda').float().unsqueeze(0), randomize_noise=False,
+                                       return_latents=True)
             print('Inference time: {}'.format(time.time() - inference_time))
             res_img = tensor2im(res_img[0])
-
-
 
         if not os.path.exists(os.path.join(RESULT_DATA_PATH, str(i))):
             os.mkdir(os.path.join(RESULT_DATA_PATH, str(i)))
 
         res_img.save(os.path.join(res_path, '{}-encode.png'.format(i)))
         np.save(os.path.join(res_path, '{}-latent-dir.npy'.format(i)), latents_dir.data.cpu().numpy()[0])
-  
 
 
 if __name__ == "__main__":
