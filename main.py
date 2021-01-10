e124d46bbfce22cdfd9aaaa1d24931786a6be435
Vladimir Bogdan <vv.bogdan98@yandex.ru>
2021-01-10 15:27:30 +0000

    Service inference sample

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ RESULT_DATA_PATH = './test_data_res'
 LATENT_DIRECTION_PATH = './latent_directions'
 FACE_LANDMARKS_PATH = 'shape_predictor_68_face_landmarks.dat'
 ALIGN_IMAGE = True
-COEFFS = [-15., -12., -9., -6., -3., 0., 3., 6., 9., 12.]
+COEFFS = [-15, -12, -9, -6, -3, 0, 3, 6, 9, 12]
 
 AGE_LATENT_DIR = './latent_directions/age.npy'
 SMILE_LATENT_DIR = './latent_directions/smile.npy'
@@ -43,6 +43,15 @@ def main():
     num_images = len(test_images_paths)
     print('{} Images'.format(num_images))
 
+    latent_dirs = []
+    #load stylegan2 latent directions
+    age_dir = np.load(AGE_LATENT_DIR).astype('float32')
+    smile_dir = np.load(SMILE_LATENT_DIR).astype('float32')
+    gender_dir = np.load(GENDER_LATENT_DIR).astype('float32')
+
+    latent_dirs.append(age_dir)
+    latent_dirs.append(smile_dir)
+    latent_dirs.append(gender_dir)
     # Load pretraiden weights
     ckpt = torch.load(PRETRAINED_WEIGHT, map_location='cpu')
 
@@ -69,19 +78,28 @@ def main():
         aligned_image = align_face(filepath=test_images_paths[i], predictor=predictor)
         aligned_image = aligned_image.convert('RGB')
         inp_img = INFERENCE_DATA_TRANSFORMS(aligned_image)
+        if not os.path.exists(res_path):
+            os.mkdir(res_path)
 
         with torch.no_grad():
             inference_time = time.time()
-            res_img, latents_dir = net(inp_img.to('cuda').float().unsqueeze(0), randomize_noise=False,
+            enc_img, latents_dir = net(inp_img.to('cuda').float().unsqueeze(0), randomize_noise=False,
                                        return_latents=True)
-            print('Inference time: {}'.format(time.time() - inference_time))
-            res_img = tensor2im(res_img[0])
 
-        if not os.path.exists(os.path.join(RESULT_DATA_PATH, str(i))):
-            os.mkdir(os.path.join(RESULT_DATA_PATH, str(i)))
+            np_latents_dir = latents_dir.data.cpu().numpy()
+            for i, dirs in enumerate(latent_dirs):
+                dirs_res_path = os.path.join(res_path, str(i))
+                if not os.path.exists(dirs_res_path):
+                    os.mkdir(dirs_res_path)
+                for coeff in COEFFS:
+                    np_latents_dir[0][:8] = (latents_dir.data.cpu().numpy()[0] + coeff * dirs)[:8]
 
-        res_img.save(os.path.join(res_path, '{}-encode.png'.format(i)))
-        np.save(os.path.join(res_path, '{}-latent-dir.npy'.format(i)), latents_dir.data.cpu().numpy()[0])
+                    image, res_latents = net.decoder([torch.from_numpy(np_latents_dir).to('cuda').float()], 
+                                                        input_is_latent=True, 
+                                                        randomize_noise=False)
+                    image = tensor2im(image[0])
+                    image.save(os.path.join(dirs_res_path, '{}-dirs-{}-coeffs-res.png'.format(i, coeff)))    
+        #np.save(os.path.join(RESULT_DATA_PATH, '{}-latent-dir.npy'.format(i)), latents_dir.data.cpu().numpy()[0])
 
 
 if __name__ == "__main__":
